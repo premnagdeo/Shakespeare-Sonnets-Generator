959353041ed228e81d0439e98c1c8cf0b9f51e68
premnagdeo <premnagdeo@gmail.com>
2020-05-10 04:16:02 +0530

    Fourth commit

--- generate_sonnets.py
+++ generate_sonnets.py
@@ -69,7 +69,7 @@ def generate_sonnet():
     new_sonnet = data[start_idx:start_idx + maxlen]
 
     generated_sonnet = []
-    for i in range(600):
+    for i in range(300):
         # Vectorize generated text
         sampled = np.zeros((1, maxlen, len(chars)))
         for j, char in enumerate(new_sonnet):
